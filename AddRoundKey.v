08e48322d330903d8e2f075dbac604a926ce3de6
islam amr <islam.mohamed02@eng-st.cu.edu.eg>
2023-05-01 00:11:45 +0300

    Modified AddRoundKey

--- AddRoundKey.v
+++ AddRoundKey.v
@@ -1,5 +1,5 @@
 module AddRoundKey (
-	w0_curr,
+	/*w0_curr,
 	w1_curr,
 	w2_curr,
 	w3_curr,
@@ -10,15 +10,22 @@ module AddRoundKey (
 	w0_next,
 	w1_next,
 	w2_next,
-	w3_next
+	w3_next*/
+	addrk_in,
+	addrk_key,
+	addrk_out
 );
 
-input [31:0] w0_curr, w1_curr, w2_curr, w3_curr, k0_curr, k1_curr, k2_curr, k3_curr;
-output [31:0] w0_next, w1_next, w2_next, w3_next;
+//input [31:0] w0_curr, w1_curr, w2_curr, w3_curr, k0_curr, k1_curr, k2_curr, k3_curr;
+//output [31:0] w0_next, w1_next, w2_next, w3_next;
+input  [127:0]addrk_in;
+input  [127:0]addrk_key;
+output [127:0]addrk_out;
 
-assign w0_next = w0_curr ^ k0_curr;
-assign w1_next = w1_curr ^ k1_curr;
-assign w2_next = w2_curr ^ k2_curr;
-assign w3_next = w3_curr ^ k3_curr;
+//assign w0_next = w0_curr ^ k0_curr;
+//assign w1_next = w1_curr ^ k1_curr;
+//assign w2_next = w2_curr ^ k2_curr;
+//assign w3_next = w3_curr ^ k3_curr;
+assign addrk_out = addrk_in ^ addrk_key;
 
 endmodule
